d2b929eff62a1eeaf0ac110f53761ca82e33b93d
Olivia <shen.olivia.us@gmail.com>
2022-07-21 00:01:53 +0000

    Add navbar and paragraph to income-expense.html page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -19,38 +19,40 @@ google.charts.setOnLoadCallback(drawIncomeExpenseChart);
 function drawIncomeExpenseChart() {
   fetch('/process-income-expense').then(response => response.json())
   .then((incomeExpenseData) => {
-    const data = new google.visualization.DataTable();
+    // if CSV data has been submitted, create chart
+    if (Object.keys(incomeExpenseData).length !== 0) {
+      const data = new google.visualization.DataTable();
+      // Display income, expense, and cash flow for each month
+      data.addColumn('date', 'Month');
+      data.addColumn('number', 'Income');
+      data.addColumn('number', 'Expense');
+      data.addColumn('number', 'Cash Flow');
 
-    // Display income, expense, and cash flow for each month
-    data.addColumn('date', 'Month');
-    data.addColumn('number', 'Income');
-    data.addColumn('number', 'Expense');
-    data.addColumn('number', 'Cash Flow');
+      // For each month in income/expense data object
+      Object.keys(incomeExpenseData).forEach((month) => {
+        // Add row with income, expense, and cashflow
+        const income = incomeExpenseData[month].income;
+        const expense = incomeExpenseData[month].expense;
+        const cashFlow = income - expense;
+        data.addRow([new Date(month), income, expense, cashFlow]);
+      });
 
-    // For each month in income/expense data object
-    Object.keys(incomeExpenseData).forEach((month) => {
-      // Add row with income, expense, and cashflow
-      const income = incomeExpenseData[month].income;
-      const expense = incomeExpenseData[month].expense;
-      const cashFlow = income - expense;
-      data.addRow([new Date(month), income, expense, cashFlow]);
-    });
+      const options = {
+        'title': 'Income vs. Expense by Month',
+        'width':800,
+        'height':500,
+        'hAxis':{
+          'format': 'MMM y',
+          'gridlines': {'color': 'none'}
+        },
+        'vAxis': {'format': 'currency'},
+        'titleTextStyle': {'fontSize': 16}
+      };
 
-    const options = {
-      'title': 'Income vs. Expense by Month',
-      'width':800,
-      'height':500,
-      'hAxis':{
-        'format': 'MMM y',
-        'gridlines': {'color': 'none'}
-      },
-      'vAxis': {'format': 'currency'},
-      'titleTextStyle': {'fontSize': 16}
-    };
-
-    const chart = new google.visualization.ColumnChart(
-        document.getElementById('income-expense-chart'));
-    chart.draw(data, options);
+      const chart = new google.visualization.ColumnChart(
+          document.getElementById('income-expense-chart'));
+      chart.draw(data, options);
+    }
   });
 }
 
